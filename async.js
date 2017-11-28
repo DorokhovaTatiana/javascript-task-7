78efe031742cd5e9e709fdd6a227752540feb129
Dorokhova Tatiana <t.dorohovav@gmail.com>
2017-11-29 02:42:25 +0500

    done

--- async.js
+++ async.js
@@ -8,6 +8,48 @@ exports.runParallel = runParallel;
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
  */
+
+let countRunning = 0;
+let requestIndex = 0;
+let translationData = [];
+
 function runParallel(jobs, parallelNum, timeout = 1000) {
     // асинхронная магия
+    return new Promise(resolve => {
+        if (!jobs.length) {
+            resolve([]);
+        }
+
+        jobs
+            .slice(0, parallelNum)
+            .forEach((request, index) => {
+                translate(request, index);
+            });
+
+        function translate(request, index) {
+            countRunning++;
+            requestIndex++;
+            requestApply(request, timeout)
+                .then(data => followingRequest(data, index));
+        }
+
+        function followingRequest(data, index) {
+            countRunning--;
+            translationData[index] = data;
+            if (requestIndex === jobs.length) {
+                resolve(translationData);
+            }
+            if (countRunning < parallelNum) {
+                translate(jobs[requestIndex], requestIndex);
+            }
+        }
+    });
+}
+
+function requestApply(request, timeout) {
+    return new Promise((resolve, reject) => {
+        request()
+            .then(resolve, reject);
+        setTimeout(() => reject(new Error('Долго выполняется')), timeout);
+    });
 }
